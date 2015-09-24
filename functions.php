6f4b0ef919d5db9209d3b478e424fcf2b9e58411
Adrien Goulet <ace@sanbornmediafactory.com>
2015-09-24 16:16:20 -0400

    adding post types functions file

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 // Requirements
 require(get_template_directory().'/inc/vars.php');
 //require(get_template_directory().'/inc/taxonomies.php');
+//require(get_template_directory().'/inc/post_types.php');
 require(get_template_directory().'/inc/acf.php');
 require(get_template_directory().'/inc/misc_functions.php');
 require(get_template_directory().'/inc/images.php');
---
+++ inc/post_types.php
@@ -0,0 +1,42 @@
+<?php
+
+if ( ! function_exists( 'smf_reg_post_types' ) ) {
+
+    add_action('init', 'smf_reg_post_types');
+    
+    function smf_reg_post_types(){
+        $labels = array(
+    		'name' => _x('Portfolio Items', 'post type general name'),
+    		'singular_name' => _x('Portfolio Item', 'post type singular name'),
+    		'add_new' => _x('Add New Portfolio Item', 'bdes'),
+    		'add_new_item' => __('Add New Portfolio Item'),
+    		'edit_item' => __('Edit Portfolio Item'),
+    		'new_item' => __('New Portfolio Item'),
+    		'view_item' => __('View Portfolio Item'),
+    		'search_items' => __('Search Portfolio Items'),
+    		'not_found' =>  __('Nothing found'),
+    		'not_found_in_trash' => __('Nothing found in Trash'),
+    		'parent_item_colon' => ''
+    	);
+         
+    	$args = array(
+    		'labels' => $labels,
+    		'label'	=> 'Portfolio Items',
+    		'public' => true,
+    		'publicly_queryable' => true,
+    		'description' => 'Portfolio Item',
+    		'show_ui' => true,
+    		'query_var' => true,
+    		'capability_type' => 'post',
+    		'hierarchical' => false,
+//     		'menu_position' => $menu_pos,
+    		'supports' => array('title','thumbnail','editor','revisions'),
+    		'taxonomies' => array()
+        ); 
+     
+        register_post_type( 'portfolio' , $args );          
+    }
+    
+}
+
+?>
