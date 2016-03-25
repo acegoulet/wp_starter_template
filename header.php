e9763a7162a39081209218b2323ed559316b692a
Adrien Goulet <ace@sanbornmediafactory.com>
2016-03-25 08:33:07 -0700

    Removing some old browser hacks for browsers we do not support anymore.

--- header.php
+++ header.php
@@ -1,22 +1,16 @@
 <?php // my_force_login(); /* Uncomment to enable the force login function */ ?>
 
 <!DOCTYPE html>
-<!--[if lt IE 7]><html class="lt-ie9 lt-ie8 lt-ie7" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
-<!--[if IE 7]><html class="lt-ie9 lt-ie8" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
-<!--[if IE 8]><html class="lt-ie9" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
-<!--[if IE 9]><html class="ie9" lang="en" xmlns="http://www.w3.org/1999/xhtml"> <![endif]-->
-<!--[if gt IE 9]><!--> <html lang="en" class="gt-ie9" xmlns="http://www.w3.org/1999/xhtml"> <!--<![endif]-->
+<html lang="en" class="gt-ie9" xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php wp_title(' &mdash; ',true,'right'); ?><?php bloginfo('name'); ?></title>
-	<?php if ( is_home() ) {?><meta name="description" content="<?php bloginfo('description'); ?>"><?php } ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/favicon.ico" />
 	<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/img/appletouch-114x114.jpg" />
 	<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/img/appletouch-144x144.jpg" />
 	<?php wp_head(); ?>
-	<!--[if lt IE 9]><script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
