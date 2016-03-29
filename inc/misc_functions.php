cfa029fc51e5f9cd2de20cd0a1e4b7bae8f3770f
Adrien Goulet <ace@sanbornmediafactory.com>
2016-03-29 07:22:43 -0700

    cleaning up the header output a bit more

--- inc/misc_functions.php
+++ inc/misc_functions.php
@@ -233,6 +233,15 @@ remove_action('wp_head','feed_links_extra', 3);
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 add_filter('xmlrpc_enabled', '__return_false');
 remove_action('wp_head', 'rsd_link'); //Edit URI Link (xmlrpc)
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+
+//remove widget css ouput from header
+add_action( 'widgets_init', 'my_remove_recent_comments_style' );
+function my_remove_recent_comments_style() {
+	global $wp_widget_factory;
+	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'  ) );
+}
 
 //remove emoji support
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
