462a6e690998cee108025d085b2162dc8a29d856
acegoulet <ace@acegoulet.com>
2017-10-07 11:40:15 -0700

    updating app icons and favicons

--- header.php
+++ header.php
@@ -5,9 +5,16 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php wp_title(' &mdash; ',true,'right'); ?><?php bloginfo('name'); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
-	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/favicon.ico" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/img/appletouch-114x114.jpg" />
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/img/appletouch-144x144.jpg" />
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/img/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/img/favicon-16x16.png">
+	<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/img/manifest.json">
+	<link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/img/safari-pinned-tab.svg" color="#ffffff">
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/favicon.ico">
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>">
+	<meta name="application-name" content="<?php bloginfo('name'); ?>">
+	<meta name="msapplication-config" content="<?php bloginfo('template_directory'); ?>/img/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
 	<?php wp_head(); ?>
 </head>
 
